abf5f48834c3803eb1f707bd779ae1c9978fadc5
Miliey <ahoussoumiliey@gmail.com>
2026-07-14 23:28:58 +0100

    Enhanced Animal class to include diet and food cost attributes. Updated add_animal function to capture these details. Introduced Visitor class for managing visitor information and ticket purchases. Refactored Zoo class to handle visitor registration and display. Improved data handling for animals and enclosures.

--- main.py
+++ main.py
@@ -21,11 +21,13 @@ class enclosure:
     
 nature = enclosure("Fields", 99999)
 class Animal:
-    def __init__(self, name, age, weight, enclosure=nature):
+    def __init__(self, name, age, weight, diet, foodcost,  enclosure=nature):
         self.name = name
         self.age = age
         self.weight = weight
         self.enclosure = enclosure.name
+        self.diet = diet
+        self.foodcost = foodcost
     def eat(self):
         print(self.name,"is eating")
         return 0
@@ -69,46 +71,77 @@ class Bird(Animal):
         super().display()
         print("Type: Bird")
 
-def add_animal(enclosure):
+def add_animal(self,enclosure):
     print("Which type of animal do you want to add?")
     choix = input("1.Lion \n2.Elephant \n3.Monkey \n4.Bird\n")
     Name = input("Enter the Animal Name : ")
     Age = int(input("Enter the Animal Age : "))
     Weight = int(input("Enter the Animal Weight(kg) : "))
+    Diet = input("Enter the Animal Diet : ")
+    Foodcost = int(input("Enter the Animal Food cost(€) : "))
     if choix == "1":
-        NameN = Lion(Name, Age, Weight,enclosure)
-        Animals_list.append(NameN)
+        NameN = Lion(Name, Age, Weight, Diet, Foodcost, enclosure)
+        self.Animals_list.append(NameN)
     elif choix == "2":
-        NameN = Elephant(Name, Age, Weight,enclosure)
-        Animals_list.append(NameN)
+        NameN = Elephant(Name, Age, Weight, Diet, Foodcost,enclosure)
+        self.Animals_list.append(NameN)
     elif choix == "3":
-        NameN = Monkey(NameN)
-        Animals_list.append(NameN)
+        NameN = Monkey(Name, Age, Weight, Diet, Foodcost, enclosure)
+        self.Animals_list.append(NameN)
     elif choix == "4":
-        NameN = Bird(Name, Age, Weight,enclosure)
-        Animals_list.append(NameN)
+        NameN = Bird(Name, Age, Weight, Diet, Foodcost,enclosure)
+        self.Animals_list.append(NameN)
     else :
         print("choice not correct")
         add_animal(enclosure)
     enclosure.add_animal_to_enclosure(NameN)
 
+class visitor:
+    def __init__(self, id, Name, ticket=False):
+        self.id = id
+        self.Name = Name
+        self.ticket = ticket
+    
+    def display_visitors_informations(self):
+        print("------------------------------------")
+        print("ID:", self.id)
+        print("Name:", self.Name)
+        print("Ticket:", self.ticket)
+
+    def buy_ticket(self):
+        if self.ticket == False:
+            self.ticket = True
+
 class Zoo():
 
-    id = 0
+    def __init__(self):
+        self.id_visitor = 0
+        self.visitor_list = []
+        self.Enclosure_list = []
+        self.Animals_list = [] 
+        self.Visitor_list = []
+        Lion1 = Lion("Marco", 4, 241, "skinny", 2536)
+        Monkey1 = Monkey("Monkey", 8, 35, "skinny", 2536)
+        self.Animals_list.append(Monkey1)
+        self.Animals_list.append(Lion1)
+        
+        
+    Lion1 = Lion("Marco", 4, 241, "skinny", 2536)
+    Monkey1 = Monkey("Monkey", 8, 35, "skinny", 2536)
     def add(self):
-        if Enclosure_list != []:
+        if self.Enclosure_list != []:
             print("Type the number of the enclosure of destination")
             number = 1
-            for i in Enclosure_list:
+            for i in self.Enclosure_list:
                 print(number,":", i.name)
                 number += 1
             enclosure_number = int(input(">"))
             if enclosure_number > number:
                 print("Number not correct")
-            elif Enclosure_list[enclosure_number-1].quantity <= 0 :
+            elif self.Enclosure_list[enclosure_number-1].quantity <= 0 :
                 print("This enclosure is full")
             else :
-                enclosure = Enclosure_list[enclosure_number-1]
+                enclosure = self.Enclosure_list[enclosure_number-1]
                 add_animal(enclosure)
         else:
             print("You don't have any enclosure")
@@ -118,10 +151,10 @@ class Zoo():
         print("Enter your enclosure information")
         name = input("Enter the Name of the enclosure :")
         quantity = int(input("Enter the Quantity of the enclosure :"))
-        Enclosure_list.append(enclosure(name,quantity))
+        self.Enclosure_list.append(enclosure(name,quantity))
     
     def display_all_enclosure_information(self):
-        for i in Enclosure_list:
+        for i in self.Enclosure_list:
             print("------------------------------------")
             print("Name : ",i.name)
             print("Quantity : ",i.quantity)
@@ -129,9 +162,9 @@ class Zoo():
     def supprimer(self):
         Name_delete = input("Enter the name of the animal you want to delete : ")
         find = False
-        for i in Animals_list:
+        for i in self.Animals_list:
             if i.name == Name_delete:
-                Animals_list.remove(i)
+                self.Animals_list.remove(i)
                 find = True
         if find is False:
             print("This animal Does not exist")
@@ -139,7 +172,7 @@ class Zoo():
     def rechercher(self):
         Name_search = input("Enter the name of the animal you are looking for : ")
         find = False
-        for i in Animals_list:
+        for i in self.Animals_list:
             if i.name == Name_search:
                 i.display()
                 find = True
@@ -147,22 +180,47 @@ class Zoo():
             print("This animal Does not exist")
 
     def afficher(self):
-        for i in Animals_list:
+        for i in self.Animals_list:
                 i.display()
-        if Animals_list == []:
+        if self.Animals_list == []:
             print("The list is empty")
 
-    def nourrir_animaux(Animal):
-        for i in Animals_list:
+    def nourrir_animaux(self):
+        for i in self.Animals_list:
                 i.eat()
-        if Animals_list == []:
+        if self.Animals_list == []:
             print("The list is empty")
 
-    def faire_parler(Animal):
-        for i in Animals_list:
+    def faire_parler(self):
+        for i in self.Animals_list:
                 i.make_noise()
-        if Animals_list == []:
+        if self.Animals_list == []:
             print("The list is empty")
+
+    def add_visitor(self):
+        print("What is your name ?")
+        Visitor_name = input(">")
+        Visitor_name = visitor(self.id_visitor,Visitor_name)
+        self.Visitor_list.append(Visitor_name)
+        print("Registration successfull your ID is ", self.id_visitor)
+        self.id_visitor += 1
+        
+
+    def display_all_visitor(self):
+        for i in self.Visitor_list:
+            i.display_visitors_informations()
+
+    def buy_ticket(self):
+        visitor_id = input("What is your visitor ID ?: ")
+        find = False
+        for i in self.visitor_list:
+            if i.id == visitor_id:
+                i.buy_ticket()
+                find = True
+        if find is False:
+            print("This visitor ID does not exist")
+
+    
 class Employee:
     def __init__(self, name, salaire):
         self.name = name
@@ -182,13 +240,6 @@ class Keeper(Employee):
     def work(self):
         print("Check for animals")
 
-        
-Lion1 = Lion("Marco", 4, 241)
-Monkey1 = Monkey("Monkey", 8, 35)
-Enclosure_list = []
-Animals_list = []
-Animals_list.append(Monkey1)
-Animals_list.append(Lion1)
 zoo = Zoo()
 def main():
     print("Welcome to the Zoo management system")
@@ -204,6 +255,9 @@ def main():
         print("6.Make all animals sound")
         print("7.Add an enclosure")
         print("8.Print all enclosure information")
+        print("9.Add a visitor")
+        print("10.Display visitors information")
+        print("11.Buy a ticket")
         choice = input(">")
         if choice == "1":
             zoo.afficher()
@@ -221,10 +275,14 @@ def main():
             zoo.create_enclosure()
         elif choice == "8":
             zoo.display_all_enclosure_information()
+        elif choice == "9":
+            zoo.add_visitor()
+        elif choice == "10":
+            zoo.display_all_visitor()
+        elif choice == "10":
+            zoo.buy_ticket()
         elif choice == "0":
             print("Aurevoir!")
         else :
             print("choix incorrect")
 main()
-
-print(Animals_list)
